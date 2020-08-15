e5909390e7eacf11c11414b60acf07f3b9cb5a21
Collin Bachman <collin.bachman@gmail.com>
2020-08-15 15:33:32 -0400

    Add GTA Vehicle info and link to Google Sheet

--- src/pages/gaming/gta-online.js
+++ src/pages/gaming/gta-online.js
@@ -6,7 +6,7 @@ import SEO from "../../components/seo"
 
 export const query = graphql`
 query GTAQuery {
-  gta_wishlist: allGoogleSheetGameStatsGtaWishList {
+  wishlist: allGoogleSheetGameStatsGtaWishList {
     nodes {
       item
       buyAt
@@ -29,6 +29,20 @@ query GTAQuery {
   grinddays: googleSheetGameStatsGtaSummary(summaryTitle: {eq: "Grind Days Remaining"}) {
     summaryAmount
   }
+  vehicleworth: googleSheetGameStatsGtaSummary(summaryTitle: {eq: "Total Vehicle Value"}) {
+    summaryAmount
+  }
+  vehicles: allGoogleSheetGameStatsGtaVehicles(sort: {fields: floor, order: ASC}) {
+    locations: group(field: location) {
+      location: fieldValue
+      nodes {
+        floor
+        vehicle
+        cost
+        gtaWikiLink
+      }
+    }
+  }
 }
 
 `
@@ -41,6 +55,13 @@ const GTAOnline = ({ data }) => (
                 <div className="col">
                     <h1 className="display-1">GTA Online</h1>
                     <h2>Updated at Least Once a Week</h2>
+                    <button className="btn btn-primary">
+                        <a
+                            href="https://docs.google.com/spreadsheets/d/1OfEJQjGGILU4P0A4W5OM49qULxSevWhxwCwMJWD2hU8/edit?usp=sharing"
+                        target="_blank" rel="nofollow noreferrer">
+                            Source: Google Sheet
+                        </a>
+                    </button>
                 </div>
             </div>
             <hr />
@@ -81,10 +102,53 @@ const GTAOnline = ({ data }) => (
                             <th scope="row">Grind Days Remaining</th>
                             <td>{ data.grinddays.summaryAmount }</td>
                         </tr>
+                        <tr>
+                            <th scope="row">Total Vehicle Value</th>
+                            <td>{ data.vehicleworth.summaryAmount.toLocaleString(
+                                'en-us',
+                                { style: "currency", currency: "USD", minimumFractionDigits: 0 }
+                            )}</td>
+                        </tr>
                     </table>
                 </div>
             </div>
-            <div className="row">
+            <div className="row mb-5">
+                <div className="col">
+                    <h2>Vehicles</h2>
+                    <p>These are all the vehicles I own in the game and their worth.</p>
+                    { data.vehicles.locations.map( l => (
+                        <>
+                            <h3>{ l.location }</h3>
+                            <table className="table">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Floor</th>
+                                    <th scope="col">Vehicle</th>
+                                    <th scope="col">Cost</th>
+                                    <th scope="col">More Info</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                { l.nodes.map(v => (
+                                    <tr>
+                                        <td>{ v.floor }</td>
+                                        <td>{ v.vehicle }</td>
+                                        <td>{ v.cost.toLocaleString(
+                                            'en-us',
+                                            { style: "currency", currency: "USD", minimumFractionDigits: 0 }
+                                        )}</td>
+                                        <td>
+                                            <a href={ v.gtaWikiLink } target="_blank" rel="nofollow noreferrer">GTA Wiki</a>
+                                        </td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        </>
+                    ))}
+                </div>
+            </div>
+            <div className="row mb-5">
                 <div className="col">
                     <h2>Wish List</h2>
                     <p>If an item has a negative Days to Grind, it means I can afford it, but haven't bought it yet in favor of saving up for a different item.</p>
@@ -98,7 +162,7 @@ const GTAOnline = ({ data }) => (
                         </tr>
                         </thead>
                         <tbody>
-                        { data.gta_wishlist.nodes.map(g => (
+                        { data.wishlist.nodes.map(g => (
                             <tr>
                                 <th scope="row">{ g.item }</th>
                                 <td>{ g.buyAt }</td>
